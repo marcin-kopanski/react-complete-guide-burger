8a3ca076448c66db1a7d010810eaf2fb23b8a3f6
Marcin Kopański <marcin.kopanski@gmail.com>
2018-04-15 15:54:59 +0200

    burder ingredients dynamicly, burger summary, modal and backdrop

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+	<div className={classes.buildControl}>
+		<div className={classes.label}>{props.label}</div>
+		<button onClick={props.removed} className={classes.less} disabled={props.disabled}>Less</button>
+		<button onClick={props.added} className={classes.more}>More</button>
+	</div>
+);
+
+export default buildControl;
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -10,32 +10,32 @@ class BurgerIngredient extends Component {
 
 		switch (this.props.type) {
 			case('bread-bottom'):
-				ingredient = <div className={classes.BreadBottom}>this.props.type</div>;
+				ingredient = <div className={classes.BreadBottom}></div>;
 				break;
 
 			case('bread-top'):
 				ingredient = (
 					<div className={classes.BreadTop}>
-						<div className={classes.Seeds1}>this.props.type</div>
-						<div className={classes.Seeds2}>this.props.type</div>
+						<div className={classes.Seeds1}></div>
+						<div className={classes.Seeds2}></div>
 					</div>
 				);
 				break;
 
 			case('meat'):
-				ingredient = <div className={classes.Meat}>this.props.type</div>;
+				ingredient = <div className={classes.Meat}></div>;
 				break;
 
 			case('cheese'):
-				ingredient = <div className={classes.Cheese}>this.props.type</div>;
+				ingredient = <div className={classes.Cheese}></div>;
 				break;
 
 			case('bacon'):
-				ingredient = <div className={classes.Bacon}>this.props.type</div>;
+				ingredient = <div className={classes.Bacon}></div>;
 				break;
 
 			case('salad'):
-				ingredient = <div className={classes.Salad}>this.props.type</div>;
+				ingredient = <div className={classes.Salad}></div>;
 				break;
 
 			default:
