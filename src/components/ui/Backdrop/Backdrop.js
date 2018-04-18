662efa2900c656d2f719ff2dabbb525957372f22
Marcin Kopański <marcin.kopanski@gmail.com>
2018-04-18 18:40:37 +0200

    backdrop cleanup

--- src/components/ui/Backdrop/Backdrop.js
+++ src/components/ui/Backdrop/Backdrop.js
@@ -3,7 +3,9 @@ import React from 'react';
 import classes from './Backdrop.css';
 
 const backdrop = (props) => (
-	props.show ? <div className={classes.backdrop} onClick={props.clicked}></div> : null
+	props.show ? <div className={classes.backdrop}
+					  onClick={props.clicked}></div>
+		: null
 );
 
 export default backdrop;
