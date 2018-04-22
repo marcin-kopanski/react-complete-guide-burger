898265e0206d8d5f5416c450096637942893ee22
Marcin Kopański <marcin.kopanski@gmail.com>
2018-04-22 20:51:38 +0200

    sideDrawer toggle with backdrop

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrwaer/SideDrawer';
+
+class Layout extends Component {
+	state = {
+		showSideDrawer: false
+	}
+
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+
+	slideDrawerToggleHandler = () => {
+		console.log('ok');
+		this.setState((prevState) => {
+			return {showSideDrawer: !prevState.showSideDrawer};
+		});
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Toolbar drawerToggleClicked={this.slideDrawerToggleHandler}/>
+				<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+				<main className={classes.content}>
+					{this.props.children}
+				</main>
+			</React.Fragment>
+		);
+	}
+};
+
+export default Layout;
